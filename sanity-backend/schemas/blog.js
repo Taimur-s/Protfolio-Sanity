2f1df34546ec7ab72eb11727a7c5ec7336193ef9
Taimur-s <tamur.hashmi@gmail.com>
2023-02-11 20:43:56 +0500

    ui-ux fetching data

--- sanity-backend/schemas/blog.js
+++ sanity-backend/schemas/blog.js
@@ -1,87 +1,100 @@
 // schemas/pet.js
 export default {
-    name: 'blog',
-    type: 'document',
-      title: 'Blog',
-    fields: [
-      {
-        name: 'title',
-        type: 'string',
-        title: 'Title'
-      },
-      {
-        name: 'content',
-        title: 'Content',
-        type: 'array',
-        of: [
-          {
-            type: 'block'
-          },
-          {
-            type: 'image',
-            fields: [
-              {
-                type: 'text',
-                name: 'alt',
-                title: 'Alternative text',
-                description: `Some of your visitors cannot see images, 
+  name: 'blog',
+  type: 'document',
+  title: 'Blog',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    }, 
+    {
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: input => input
+          .toLowerCase()
+          .replace(/\s+/g, '-')
+          .slice(0, 200)
+      }
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [
+        {
+          type: 'block'
+        },
+        {
+          type: 'image',
+          fields: [
+            {
+              type: 'text',
+              name: 'alt',
+              title: 'Alternative text',
+              description: `Some of your visitors cannot see images, 
                   be they blind, color-blind, low-sighted; 
                   alternative text is of great help for those 
                   people that can rely on it to have a good idea of 
                   what\'s on your page.`,
-                options: {
-                  isHighlighted: true
-                }
+              options: {
+                isHighlighted: true
               }
-            ]
-          }
-        ]
-      },
-      {
-        name: 'metsdesc',
-        type: 'string',
-        title: 'Meta Description'
-      },
-      {
-        title: 'Blog Image',
-        name: 'blogimage',
-        type: 'image',
-        options: {
-          hotspot: true // <-- Defaults to false
-        },
-        fields: [
-          {
-            name: 'caption',
-            type: 'string',
-            title: 'Caption',
-            options: {
-              isHighlighted: true // <-- make this field easily accessible
             }
-          },
-          {
-            // Editing this field will be hidden behind an "Edit"-button
-            name: 'attribution',
-            type: 'string',
-            title: 'Attribution',
-          }
-        ]
+          ]
+        }
+      ]
+    },
+    {
+      name: 'metsdesc',
+      type: 'string',
+      title: 'Meta Description'
+    },
+    {
+      title: 'Blog Image',
+      name: 'blogimage',
+      type: 'image',
+      options: {
+        hotspot: true // <-- Defaults to false
       },
-      {
-        name: 'createdAt',
-        type: 'datetime',
-        title: 'Created At'
-      },
-      {
-        name: 'author',
-        type: 'object',
-        fields: [
-          {
-            title: 'Author',
-            name: 'author',
-            type: 'reference',
-            to: [{type: 'author'}]
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Caption',
+          options: {
+            isHighlighted: true // <-- make this field easily accessible
           }
-        ]
-      }
-    ]
-  }
+        },
+        {
+          // Editing this field will be hidden behind an "Edit"-button
+          name: 'attribution',
+          type: 'string',
+          title: 'Attribution',
+        }
+      ]
+    },
+    {
+      name: 'createdAt',
+      type: 'datetime',
+      title: 'Created At'
+    },
+    {
+      name: 'author',
+      type: 'object',
+      fields: [
+        {
+          title: 'Author',
+          name: 'author',
+          type: 'reference',
+          to: [{ type: 'author' }]
+        }
+      ]
+    }
+  ]
+}
